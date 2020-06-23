b8342a95a9abe6a948f0cb16a2c5890d95fbfcc1
rickpina <rickapina@yahoo.com>
2020-06-23 17:01:17 -0400

    Bop

--- TestProblemCode/Program.cs
+++ TestProblemCode/Program.cs
@@ -184,13 +184,11 @@ namespace QuickDate
 
             //For example, given the array[0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
         }
-
         class ListForDCP77
         {
             public int A { get; set; }
             public int B { get; set; }
         }
-
         private static void DCP77()
         {
             //this problem was asked by Snapchat.
@@ -216,6 +214,7 @@ namespace QuickDate
 
 
         }
+
 
 
         static void Main(string[] args)
