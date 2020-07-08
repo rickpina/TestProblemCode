5bbc2727d39d42d7bf36e58c54ac12ee838d6e8b
rickpina <rickapina@yahoo.com>
2020-07-08 18:19:39 -0400

    ReverseString

--- TestProblemCode/Program.cs
+++ TestProblemCode/Program.cs
@@ -254,7 +254,24 @@ namespace QuickDate
             //d
         }
 
+        private static void ReverseStringWithoutInbuiltMethod(string stringInput)
+        {
+            // Reverse using While loop  
+            string reversestring = "";
+            int length;
 
+            length = stringInput.Length - 1;
+
+            while (length >= 0)
+            {
+                reversestring = reversestring + stringInput[length];
+                length--;
+            }
+            Console.WriteLine(reversestring);
+
+            Console.ReadLine();
+
+        }
 
         static void Main(string[] args)
         {
@@ -263,6 +280,7 @@ namespace QuickDate
             //DCP74MultiplicationTable();
             //DCP77();
             //Test();
+            
 
         }
 
