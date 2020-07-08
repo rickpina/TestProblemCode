1a066a2c42b756fcb0a424adacb4994a9911cb8c
rickpina <rickapina@yahoo.com>
2020-07-07 21:34:43 -0400

    Nonono

--- TestProblemCode/Program.cs
+++ TestProblemCode/Program.cs
@@ -242,6 +242,18 @@ namespace QuickDate
             }
         }
 
+        private static void DCP80()
+        {
+            //Good morning!Here's your coding interview problem for today.
+            //This problem was asked by Google.
+            //Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+            //    a
+            //   / \
+            //  b c
+            // /
+            //d
+        }
+
 
 
         static void Main(string[] args)
