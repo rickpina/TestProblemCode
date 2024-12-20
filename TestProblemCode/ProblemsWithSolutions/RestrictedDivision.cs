6d8af7729698a49dd9b2d144d4a0ac7198133693
rickpina <rickapina@yahoo.com>
2024-12-20 10:20:42 -0600

    Another one

---
+++ TestProblemCode/ProblemsWithSolutions/RestrictedDivision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProblemCode.ProblemsWithSolutions
+{
+    /// <summary>
+    /// Implement division of two positive integers without using the division, multiplication, or modulus operators.
+    /// Return the quotient as an integer, ignoring the remainder.
+    /// </summary>
+    public class RestrictedDivision
+    {
+        public static void Display()
+        {
+
+            Solve(10, 2);
+
+        }
+
+        private static void Solve(int dividend, int divisor)
+        {
+            int quotient = 0;
+
+            while (dividend > 0)
+            {
+                dividend -= divisor;
+
+                if (dividend >= 0) { quotient++; }             
+            }
+
+            Console.WriteLine(quotient);
+        }
+    }
+}
